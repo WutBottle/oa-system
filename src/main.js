f5d47c0caef2a2ba89b64e175b0320d5adf10545
WutBottle <785012476@qq.com>
2020-05-06 22:29:02 +0800

    修改路由跳转判断逻辑将main.js的判断移动到permission.js

--- src/main.js
+++ src/main.js
@@ -16,30 +16,11 @@ import echarts from 'echarts';
 Vue.prototype.$echarts = echarts;
 
 import infiniteScroll from 'vue-infinite-scroll'
-import tokensOperation from "./store/modules/tokensOperation";
 Vue.use(infiniteScroll);
 
 Vue.use(Antd);
 
 Vue.config.productionTip = false;
-
-router.beforeEach((to, from, next) => {
-  if (to.meta.requireAuth) {
-    store.dispatch('categoryOperation/getCategoryList', {categoryType: 11}).then(res => {
-      if (res && res.data.meta.success){
-        next();
-      } else {
-        router.push({
-          path: 'user/login',
-        })
-      }
-    }).catch(error => {
-      console.log(error);
-    })
-  } else {
-    next();
-  }
-});
 
 new Vue({
   store,
--- src/permission.js
+++ src/permission.js
@@ -1,41 +1,33 @@
 import router from './router'
+import store from './store'
 
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 
-import {ACCESS_TOKEN} from '@/store/mutation-types'
-
 NProgress.configure({showSpinner: false}); // NProgress Configuration
 const whiteList = ['LoginPage']; // no redirect whitelist
-const defaultRoutePath = '/main/workplace';
 
 router.beforeEach((to, from, next) => {
   NProgress.start(); // start progress bar
-  if (localStorage.getItem(ACCESS_TOKEN)) {
-    if (to.path === '/user/login') {
-      next({path: defaultRoutePath});
-      NProgress.done()
+  if (whiteList.includes(to.name)) {
+    // 在免登录白名单，直接进入
+    next();
+  } else {
+    if (to.meta.requireAuth) {
+      store.dispatch('categoryOperation/getCategoryList', {categoryType: 11}).then(res => {
+        if (res && res.data.meta.success){
+          next();
+        } else {
+          router.replace({
+            path: 'user/login',
+          });
+        }
+      }).catch(error => {
+      })
     } else {
-      // if (!store.getters.role) {
-      //   store.dispatch('permission/GenerateRoutes').then(() => {
-      //     // 根据roles权限生成可访问的路由表
-      //     // 动态添加可访问路由表
-      //     router.addRoutes(store.getters.addRouters);
-      //     next();
-      //   });
-      // } else {
-      //   next();
-      // }
       next();
     }
-  } else {
-    if (whiteList.includes(to.name)) {
-      // 在免登录白名单，直接进入
-      next()
-    } else {
-      next({path: '/user/login', query: {redirect: to.fullPath}});
-      NProgress.done() // if current page is login will not trigger afterEach hook, so manually handle it
-    }
+    NProgress.done(); // if current page is login will not trigger afterEach hook, so manually handle it
   }
 });
 
